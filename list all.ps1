deece8aadba685d952e0d22e0f8df2cd87afb377
sprnoodle <46504389+sprnoodle@users.noreply.github.com>
2019-01-18 10:06:40 -0500

    Files added
    tool script examples

---
+++ list all.ps1
@@ -0,0 +1,4 @@
+﻿$servers = #serverpath
+$installType = #type of install
+Get-ChildItem -path $servers -Recurse -Include $installType -ErrorAction SilentlyContinue | Select-Object FullName | 
+Export-Csv -path c:\users\$env:UserName\desktop\appinstalls.csv
---
+++ search.ps1
@@ -0,0 +1,29 @@
+﻿$excel = new-object -ComObject Excel.Application
+#$excel.visible = $false
+$WorkBook = $excel.Workbooks.Open('C:\Users\faghkiu\Documents\Spreadsheets\Program Paths.csv')
+$WorkSheet = $WorkBook.Sheets.Item(1)
+$WorkSheet.Name
+$Found = $WorkSheet.Cells.Find('Deploy-Application')
+$BeginAddress = $Found.Address(0,0,1,1)
+$BeginAddress
+[pscustomobject]@{
+    WorkSheet = $WorkSheet.Name
+    Column = $Found.Column
+    Row = $Found.Row
+    Text = $Found.Text
+    Address = $BeginAddress
+}
+Do {
+    $Found = $WorkSheet.Cells.FindNext($Found)
+    $Address = $Found.Address(0,0,1,1)
+    If ($Address -eq $BeginAddress){
+        Break
+    }
+    [pscustomobject]@{
+    WorkSheet = $WorkSheet.Name
+    Column = $Found.Column
+    Row = $Found.Row
+    Text = $Found.Text
+    Address = $BeginAddress
+    }
+} Until ($False)
